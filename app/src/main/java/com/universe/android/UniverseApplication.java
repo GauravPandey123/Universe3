bcca9b3a58e9b06104fa23ae918a12dbfefefec3
Gaurav Pandey <gaurav.pandey@crystal.com>
2018-01-23 01:50:31 -0800

    realm setup done

--- app/src/main/java/com/universe/android/UniverseApplication.java
+++ app/src/main/java/com/universe/android/UniverseApplication.java
@@ -3,6 +3,8 @@ package com.universe.android;
 import android.content.Context;
 
 import in.editsoft.api.util.App;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 /**
  * Created by gaurav.pandey on 22-01-2018.
@@ -16,6 +18,12 @@ public class UniverseApplication extends App {
     public void onCreate() {
         super.onCreate();
         mContext = getApplicationContext();
+        Realm.init(this);
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder()
+                .name("tasky.realm")
+                .schemaVersion(0)
+                .build();
+        Realm.setDefaultConfiguration(realmConfig);
     }
     public static Context getContext() {
         return mContext;
