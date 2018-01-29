8db81494e167aaab3e98f5756f8bf758d90c9eb4
Gaurav Pandey <gaurav.pandey@crystal.com>
2018-01-29 05:26:49 -0800

    search location search filter dialog completed

--- app/src/main/java/com/universe/android/activity/SurveyDetailActivity.java
+++ app/src/main/java/com/universe/android/activity/SurveyDetailActivity.java
@@ -4,27 +4,22 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 import com.universe.android.R;
-import com.universe.android.activity.BaseActivity;
 import com.universe.android.adapter.SurveyDetailAdapter;
-import com.universe.android.fragment.BaseFragment;
-import com.universe.android.fragment.FilterTodayFragment;
-import com.universe.android.fragment.SurveySelectionFragment;
 import com.universe.android.helper.FontClass;
-import com.universe.android.helper.PagerSlidingTabStrip;
+
+import java.util.ArrayList;
 
 /**
  * Created by gaurav.pandey on 24-01-2018.
@@ -32,7 +27,6 @@ import com.universe.android.helper.PagerSlidingTabStrip;
 
 public class SurveyDetailActivity extends BaseActivity {
     //declare the views here
-    private Dialog dialog;
 
     //decalre the variable here
     private RecyclerView recyclerViewSurveyDetail;
@@ -40,9 +34,16 @@ public class SurveyDetailActivity extends BaseActivity {
     private LinearLayoutManager linearLayoutManager;
     private SurveyDetailAdapter surveyDetailAdapter;
     private ImageView imageViewBack, imageViewFilter;
-
+    private FloatingActionMenu floatingActionMenu;
+    private FloatingActionButton floatingCrystal, floatingRetailers;
     private TextView textViewToday, textViewtarget, textViewAchievement, textViewInProgress;
     private TextView textViewNewRetailers, textViewCrystalmembers, textViewCompletedQuestionaire;
+    private TextView textViewPeriodFrom, textViewPeriodTo, textViewPeriodStatus, textViewReset, textViewApplyFilter;
+    private EditText input_period_from, input_period_to, input_period_status;
+    private  Dialog dialog;
+    private ImageView imageViewCancel;
+
+    private ArrayList<String> stringArrayList;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -63,14 +64,16 @@ public class SurveyDetailActivity extends BaseActivity {
         imageViewFilter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(mContext, FIlterActivity.class));
-                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                showDialog();
+//                startActivity(new Intent(mContext, FIlterActivity.class));
+//                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
     }
 
     private void setUpElements() {
-        surveyDetailAdapter = new SurveyDetailAdapter(mContext);
+        searchList();  // in this method, Create a list of items.
+        surveyDetailAdapter = new SurveyDetailAdapter(mContext, stringArrayList);
         linearLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerViewSurveyDetail.setLayoutManager(linearLayoutManager);
         recyclerViewSurveyDetail.setItemAnimator(new DefaultItemAnimator());
@@ -78,6 +81,7 @@ public class SurveyDetailActivity extends BaseActivity {
     }
 
     private void initialization() {
+        stringArrayList = new ArrayList<>();
         swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
         recyclerViewSurveyDetail = findViewById(R.id.recylerViewSurveyDetail);
         imageViewBack = findViewById(R.id.imageviewback);
@@ -89,6 +93,9 @@ public class SurveyDetailActivity extends BaseActivity {
         textViewNewRetailers = findViewById(R.id.textViewNewRetailers);
         textViewCrystalmembers = findViewById(R.id.textViewCrystalmembers);
         textViewCompletedQuestionaire = findViewById(R.id.textViewCompletedQuestionaire);
+        floatingActionMenu = findViewById(R.id.floating_action_menu_customers);
+        floatingCrystal = findViewById(R.id.floatingCrystal);
+        floatingRetailers = findViewById(R.id.floatingRetailers);
 
         textViewToday.setTypeface(FontClass.openSemiBold(mContext));
         textViewtarget.setTypeface(FontClass.openSansRegular(mContext));
@@ -97,7 +104,82 @@ public class SurveyDetailActivity extends BaseActivity {
         textViewNewRetailers.setTypeface(FontClass.openSansRegular(mContext));
         textViewCrystalmembers.setTypeface(FontClass.openSansRegular(mContext));
         textViewCompletedQuestionaire.setTypeface(FontClass.openSansRegular(mContext));
+
+        floatingActionMenu.setClosedOnTouchOutside(true);
+        floatingCrystal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, SearchCustomersActivity.class));
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                floatingActionMenu.close(true);
+
+            }
+        });
+
+        floatingRetailers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, SearchCustomersActivity.class));
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                floatingActionMenu.close(true);
+
+            }
+        });
+
     }
+
+    private void searchList() {
+        stringArrayList.add("Agro Inputs Corporation");
+        stringArrayList.add("Aj AgroChemicals");
+        stringArrayList.add("Blossom AgriCore");
+        stringArrayList.add("Chemical India");
+        stringArrayList.add("Duncan India");
+        stringArrayList.add("Gange Pestiside");
+        stringArrayList.add("Agro Inputs Corporation");
+        stringArrayList.add("Aj AgroChemicals");
+        stringArrayList.add("Blossom AgriCore");
+        stringArrayList.add("Chemical India");
+        stringArrayList.add("Duncan India");
+        stringArrayList.add("Gange Pestiside");
+    }
+
+    public void showDialog() {
+        // Create custom dialog object
+        dialog= new Dialog(mContext);
+        // Include dialog.xml file
+        dialog.setContentView(R.layout.filter_dialog);
+        // Set dialog title
+        dialoginitialization();
+        dialogSetUpElements();
+        dialog.show();
+
+
+    }
+
+    private void dialogSetUpElements() {
+
+    }
+
+    private void dialoginitialization() {
+        textViewPeriodFrom = dialog.findViewById(R.id.textViewPeriodFrom);
+        textViewPeriodTo = dialog.findViewById(R.id.textViewPeriodTo);
+        textViewPeriodStatus = dialog.findViewById(R.id.textViewPeriodStatus);
+        textViewReset = dialog.findViewById(R.id.textViewReset);
+        textViewApplyFilter = dialog.findViewById(R.id.textViewApplyFilter);
+        input_period_from = dialog.findViewById(R.id.input_period_from);
+        input_period_to = dialog.findViewById(R.id.input_period_to);
+        input_period_status = dialog.findViewById(R.id.input_period_status);
+
+        textViewPeriodFrom.setTypeface(FontClass.openSansRegular(mContext));
+        textViewPeriodTo.setTypeface(FontClass.openSansRegular(mContext));
+        textViewPeriodStatus.setTypeface(FontClass.openSansRegular(mContext));
+        textViewReset.setTypeface(FontClass.openSansRegular(mContext));
+        textViewApplyFilter.setTypeface(FontClass.openSansLight(mContext));
+        input_period_from.setTypeface(FontClass.openSansLight(mContext));
+        input_period_to.setTypeface(FontClass.openSansLight(mContext));
+        input_period_status.setTypeface(FontClass.openSansLight(mContext));
+    }
+
 
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/universe/android/adapter/QuestioniareSelectionAdapter.java
+++ app/src/main/java/com/universe/android/adapter/QuestioniareSelectionAdapter.java
@@ -2,11 +2,13 @@ package com.universe.android.adapter;
 
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -15,10 +17,11 @@ import com.github.aakira.expandablelayout.ExpandableLayoutListenerAdapter;
 import com.github.aakira.expandablelayout.ExpandableLinearLayout;
 import com.github.aakira.expandablelayout.Utils;
 import com.universe.android.R;
+import com.universe.android.activity.FIlterActivity;
+import com.universe.android.activity.SearchCustomersActivity;
 import com.universe.android.model.ItemModel;
 
 import java.util.List;
-
 
 
 public class QuestioniareSelectionAdapter extends RecyclerView.Adapter<QuestioniareSelectionAdapter.ViewHolder> {
@@ -48,7 +51,7 @@ public class QuestioniareSelectionAdapter extends RecyclerView.Adapter<Questioni
         holder.textView.setText(item.description);
         holder.expandableLayout.setInRecyclerView(true);
         holder.expandableLayout.setInterpolator(item.interpolator);
-        holder.expandableLayout.setExpanded(expandState.get(position));
+//        holder.expandableLayout.setExpanded(expandState.get(position));
         holder.expandableLayout.setListener(new ExpandableLayoutListenerAdapter() {
             @Override
             public void onPreOpen() {
@@ -70,11 +73,26 @@ public class QuestioniareSelectionAdapter extends RecyclerView.Adapter<Questioni
                 onClickButton(holder.expandableLayout);
             }
         });
+
+        holder.lianearLayoutCrystalCustomers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.startActivity(new Intent(context, SearchCustomersActivity.class));
+                // context.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+        holder.lianearLayoutReailters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.startActivity(new Intent(context, SearchCustomersActivity.class));
+            }
+        });
     }
 
     private void onClickButton(final ExpandableLayout expandableLayout) {
         expandableLayout.toggle();
     }
+
 
     @Override
     public int getItemCount() {
@@ -84,6 +102,7 @@ public class QuestioniareSelectionAdapter extends RecyclerView.Adapter<Questioni
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView textView;
         public RelativeLayout buttonLayout;
+        public LinearLayout lianearLayoutCrystalCustomers, lianearLayoutReailters;
         /**
          * You must use the ExpandableLinearLayout in the recycler view.
          * The ExpandableRelativeLayout doesn't work.
@@ -92,9 +111,12 @@ public class QuestioniareSelectionAdapter extends RecyclerView.Adapter<Questioni
 
         public ViewHolder(View v) {
             super(v);
-            textView = (TextView) v.findViewById(R.id.textViewReatilers);
-            buttonLayout = (RelativeLayout) v.findViewById(R.id.button);
-            expandableLayout = (ExpandableLinearLayout) v.findViewById(R.id.expandableLayout);
+            textView = v.findViewById(R.id.textViewReatilers);
+            buttonLayout = v.findViewById(R.id.button);
+            expandableLayout = v.findViewById(R.id.expandableLayout);
+            lianearLayoutCrystalCustomers = v.findViewById(R.id.lianearLayoutCrystalCustomers);
+            lianearLayoutReailters = v.findViewById(R.id.lianearLayoutReailters);
+
         }
     }
 
